82df99e7692116fdd55191e3ceca8d7dfe2d72c3
Realist <107952898+Realist2@users.noreply.github.com>
2026-02-15 06:55:36 -0400

    Refactor input validation functions for consistency

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ const validInput = () =>{
 
     }
 }
+const emailValidation = () =>{
+    if(document.querySelector('#email').value.length < 10){
+        document.querySelector('.btn').style.background = '';
+        document.querySelector('.msg').innerText = 'Invalid Characters';
+    }else {
+        document.querySelector('.btn').style.background = 'blue';
+        document.querySelector('.msg').innerText = '';
+
+    }
+}
+const registerValidation = () =>{
+    if(document.querySelector('#email').value.length < 10 || document.querySelector('#password').value.length <6 ){
+        document.querySelector('#btn').style.background = '';
+        document.querySelector('.msg').innerText = 'Invalid Characters';
+    }else {
+        document.querySelector('#btn').style.background = 'blue';
+        document.querySelector('.msg').innerText = '';
+
+    }
+}
